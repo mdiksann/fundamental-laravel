aba14303d9a79f93abcc1c057a63730498e26d1c
iksan <mdiksann@gmail.com>
2025-10-27 14:52:40 +0700

    feat: Add create and edit views for 'barang' resource

---
+++ resources/views/barang/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('content')
+<h4>Ubah Barang</h4>
+
+<form action="{{ route('barang.update', $barang->id) }}" method="post">
+    {{ csrf_field() }}
+    {{ method_field('PUT') }}
+    <div class="form-group">
+        <label>Nama Barang</label>
+        <input type="text" name="nama_barang" class="form-control" value="{{ old('nama_barang', $barang->nama_barang) }}">
+        @if ($errors->has('nama_barang')) <span class="text-danger">{{ $errors->first('nama_barang') }}</span> @endif
+    </div>
+    <div class="form-group">
+        <label>Satuan</label>
+        <input type="text" name="satuan" class="form-control" value="{{ old('satuan', $barang->satuan) }}">
+        @if ($errors->has('satuan')) <span class="text-danger">{{ $errors->first('satuan') }}</span> @endif
+    </div>
+    <div class="form-group">
+        <label>Harga</label>
+        <input type="number" step="0.01" name="harga" class="form-control" value="{{ old('harga', $barang->harga) }}">
+        @if ($errors->has('harga')) <span class="text-danger">{{ $errors->first('harga') }}</span> @endif
+    </div>
+    <div class="form-group">
+        <label>Stok</label>
+        <input type="number" name="stok" class="form-control" value="{{ old('stok', $barang->stok) }}">
+        @if ($errors->has('stok')) <span class="text-danger">{{ $errors->first('stok') }}</span> @endif
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+        <a href="{{ route('barang.index') }}" class="btn btn-secondary">Batal</a>
+    </div>
+</form>
+
+@endsection
